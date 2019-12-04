89c4c8a2dfc3a1a8e5ee565127f97d321975f62c
Drew <drewsup123c@gmail.com>
2019-12-04 15:07:35 -0700

    got redux setup in index.js

--- blockstash/src/index.js
+++ blockstash/src/index.js
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.js';
+// Redux Imports
+import {createStore, applyMiddleware} from 'redux';
+import {Privider} from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+// Setting up Redux
+const store = createStore(
+    reducer,
+    applyMiddleware(thunk, logger)
+);
 
 // Require Sass file so webpack can build it
 // import 'bootstrap/dist/css/bootstrap.css';
 // import'./styles/style.css';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root')
+);
