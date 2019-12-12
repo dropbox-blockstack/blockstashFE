0b602169a14249b2f8b227848c186a7b3519d0b0
Drew <drewsup123c@gmail.com>
2019-12-11 21:02:44 -0700

    some work on the application bar and side menu

--- blockstash/src/Components/Global/SideMenu.js
+++ blockstash/src/Components/Global/SideMenu.js
@@ -10,6 +10,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Paper from '@material-ui/core/Paper';
+import { CssBaseline, AppBar, Toolbar, Typography } from '@material-ui/core';
 
 class SideMenu extends React.Component{
 
@@ -23,21 +24,31 @@ class SideMenu extends React.Component{
     render(){
         const { classes } = this.props;
         return(
-            <Drawer className={classes.drawer} variant="permanent" anchor="left">
-                <Paper>
-                    <List>
-                        <ListItem button >
-                            <ListItemText>Hello World</ListItemText>
-                        </ListItem>
-                        <ListItem button >
-                            <ListItemText>Hello World</ListItemText>
-                        </ListItem>
-                        <ListItem button >
-                            <ListItemText>Hello World</ListItemText>
-                        </ListItem>
-                    </List>
-                </Paper>
-            </Drawer>
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar className={classes.appBar} position="fixed">
+                    <Toolbar>
+                        <Typography variant="h6" noWrap>
+                            Permanent drawer
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer className={classes.drawer} variant="permanent" anchor="left">
+                    <Paper>
+                        <List className={classes.list}>
+                            <ListItem button >
+                                <ListItemText>Hello World</ListItemText>
+                            </ListItem>
+                            <ListItem button >
+                                <ListItemText>Hello World</ListItemText>
+                            </ListItem>
+                            <ListItem button >
+                                <ListItemText>Hello World</ListItemText>
+                            </ListItem>
+                        </List>
+                    </Paper>
+                </Drawer>
+            </div>
         )
     }
 }
--- blockstash/src/styles/sideMenuStyles.js
+++ blockstash/src/styles/sideMenuStyles.js
@@ -1,9 +1,18 @@
+const drawerWidth = 300;
+
 const styles = {
+    root : {
+        display : "flex",
+    },
+    appBar : {
+        width : `calc(100% - ${drawerWidth}px)`,
+        marginLeft : drawerWidth
+    },
     drawer : {
-        width : "600px",
+        width : drawerWidth,
         // borderRight : "1px solid black"
-        borderRight : "1px solid black"
-    }
+        borderRight : "1px solid black",
+    },
 }
 
 export default styles;
