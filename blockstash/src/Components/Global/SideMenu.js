e64173b88d8f55acc3589f1335d89bcb6b735aab
jeremy <jtvtank22@gmail.com>
2019-12-18 22:34:12 -0700

    changing project to use bootstrap instead

--- blockstash/src/Components/Global/SideMenu.js
+++ blockstash/src/Components/Global/SideMenu.js
@@ -1,16 +1,4 @@
 import React from 'react';
-import styles from '../../styles/sideMenuStyles';
-import PropTypes from 'prop-types';
-// Material UI Imports
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import { withStyles } from '@material-ui/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Paper from '@material-ui/core/Paper';
-import { CssBaseline, AppBar, Toolbar, Typography } from '@material-ui/core';
 
 class SideMenu extends React.Component{
 
@@ -25,36 +13,9 @@ class SideMenu extends React.Component{
         const { classes } = this.props;
         return(
             <div className={classes.root}>
-                <CssBaseline />
-                <AppBar className={classes.appBar} position="fixed">
-                    <Toolbar>
-                        <Typography variant="h6" noWrap>
-                            Permanent drawer
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-                <Drawer className={classes.drawer} variant="permanent" anchor="left">
-                    <Paper>
-                        <List className={classes.list}>
-                            <ListItem button >
-                                <ListItemText>Hello World</ListItemText>
-                            </ListItem>
-                            <ListItem button >
-                                <ListItemText>Hello World</ListItemText>
-                            </ListItem>
-                            <ListItem button >
-                                <ListItemText>Hello World</ListItemText>
-                            </ListItem>
-                        </List>
-                    </Paper>
-                </Drawer>
             </div>
         )
     }
 }
 
-SideMenu.propTypes = {
-    classes : PropTypes.object.isRequired,
-}
-
-export default withStyles(styles)(SideMenu);
+export default SideMenu;
